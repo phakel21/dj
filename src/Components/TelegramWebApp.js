19e5881d2ded5b4a835437873f071cb2a6b407be
phakel <viktor@gmail.com>
2024-08-11 19:27:09 +0200

    add3

--- src/Components/TelegramWebApp.js
+++ src/Components/TelegramWebApp.js
@@ -9,28 +9,24 @@ const TelegramWebApp = () => {
 
     useEffect(() => {
         if (tele) {
-
             tele.ready();
-
-
-
-
             setVersion(tele.version);
-
+    
+            console.log("initDataUnsafe:", tele.initDataUnsafe); // Add this line to inspect the initDataUnsafe
+    
             const user = tele.initDataUnsafe?.user;
+            console.log("User Info:", user); // Add this line to inspect the user object
+    
             if (user) {
                 setUsername(user.username || user.first_name || 'No username available');
             } else {
                 setUsername('No user information available');
             }
-
         } else {
             setUsername('Telegram WebApp is not available');
         }
-
-
-
     }, []);
+    
 
     return (
         <div>
